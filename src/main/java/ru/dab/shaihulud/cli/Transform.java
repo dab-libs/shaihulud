1ce33804de5eb6a20ae96ce9ec4fd368eb51254a
Dmitry Bystrov <bystrov.yoso@gmail.com>
2022-01-23 14:01:49 +0300

    Точка входа для преобразователя

---
+++ src/main/java/ru/dab/shaihulud/cli/Transform.java
@@ -0,0 +1,39 @@
+package ru.dab.shaihulud.cli;
+
+import org.json.JSONObject;
+import org.json.JSONString;
+import ru.dab.shaihulud.Console;
+import ru.dab.shaihulud.io.ReaderFactory;
+import ru.dab.shaihulud.specification.JsonParser;
+import ru.dab.shaihulud.specification.Parser;
+import ru.dab.shaihulud.specification.ParserException;
+import ru.dab.shaihulud.transfomer.Transformer;
+import ru.dab.shaihulud.transfomer.jmespath.JmesPathTransformer;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class Transform {
+  public static void main(String[] args) throws IOException, ParserException {
+    Parser parser = new JsonParser(null);
+    final ReaderFactory readerFactory = new ReaderFactory();
+    final Map<String, Object> data =
+        parser.parse(readerFactory.create(args[1]));
+    final Transformer transformer =
+        new JmesPathTransformer(new Console());
+    final Object transformedData =
+        transformer.transform(readerFactory.create(args[0]), data);
+
+    String jsonString;
+    if (transformedData instanceof Map) {
+      jsonString = new JSONObject((Map<?, ?>) transformedData).toString(2);
+    }
+    else if (transformedData instanceof String) {
+      jsonString = "\"" + transformedData + "\"";
+    }
+    else {
+      jsonString = transformedData.toString();
+    }
+    System.out.println(jsonString);
+  }
+}
